ee6161a2fc92f1a2124720d18f45d019d611c5db
Deokgi Yang <corey5361@nate.com>
2024-04-08 19:17:07 +0900

    7.이진트리 순회(넓이우선탐색 : 레벨 탐색)

--- inflearn/07_Recursive_Tree_Graph/CodingTest07.java
+++ inflearn/07_Recursive_Tree_Graph/CodingTest07.java
@@ -3,14 +3,6 @@ package Recursive_Tree_Graph;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Node{
-    int data;
-    Node lf, rt;
-    public Node(int val){
-        data = val;
-        lf=rt=null;
-    }
-}
 
 public class CodingTest07 {
         Node root;
@@ -20,10 +12,35 @@ public class CodingTest07 {
             q.add(root);
             // L : Level
             int L = 0;
+            while (!q.isEmpty()){
+                //q에 원소가 몇 개 들어가 있는가?
+                int len = q.size();
+                //System.out.print(L+" : ");
+                for (int i = 0; i < len; i++) {
+                    //Node 형
+                    Node cur = q.poll();
+                    // 한 레벨의 원소 출력 후
+                    System.out.print(cur.data+" ");
+                    //자식을 q에 넣음, cur:현재노드, if(cur.lf != null) : 말단 노드 거르기 위함
+                    if(cur.left!=null) q.add(cur.left);
+                    if(cur.right!=null) q.add(cur.right);
+                }
+                //레벨이 끝난 것이므로 ++
+                L++;
+                //줄바꿈
+                System.out.println();
+            }
         }
     public static void main(String[] args) {
         CodingTest07 main = new CodingTest07();
         main.root = new Node(1);
+        main.root.left = new Node(2);
+        main.root.right = new Node(3);
+        main.root.left.left = new Node(4);
+        main.root.left.right = new Node(5);
+        main.root.right.left = new Node(6);
+        main.root.right.right = new Node(7);
+        main.BFS(main.root);
     }
 }
 /*7.이진트리 순회(넓이우선탐색 : 레벨 탐색)
